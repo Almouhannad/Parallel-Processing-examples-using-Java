228cfdfae852fe056d27130b67835af5ee78b00c
Almouhannad <almouhannad.hafez@gmail.com>
2024-09-30 21:21:30 +0300

    Add constants classes

---
+++ ImageRecolor/src/main/java/Constants/ColorsDeltas.java
@@ -0,0 +1,7 @@
+package Constants;
+
+public class ColorsDeltas {
+    public static final int redDelta = 10;
+    public static final int greenDelta = -80;
+    public static final int blueDelta = -20;
+}
---
+++ ImageRecolor/src/main/java/Constants/FilesLocations.java
@@ -0,0 +1,6 @@
+package Constants;
+
+public class FilesLocations {
+    public static final String SOURCE_FILE = "./resources/many-flowers.jpg";
+    public static final String DESTINATION_FILE = "./out/many-flowers.jpg";
+}
