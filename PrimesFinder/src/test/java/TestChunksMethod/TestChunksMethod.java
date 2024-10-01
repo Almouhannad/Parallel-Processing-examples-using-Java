85e0ca4050c82073020913f0b7a0f0cf825d1b9d
Almouhannad <almouhannad.hafez@gmail.com>
2024-10-01 22:27:02 +0300

    Update tests

--- PrimesFinder/src/test/java/TestChunksMethod/TestChunksMethod.java
+++ PrimesFinder/src/test/java/TestChunksMethod/TestChunksMethod.java
@@ -2,40 +2,33 @@ package TestChunksMethod;
 
 import PrimesFinder.Abstractions.IPrimesFinder;
 import PrimesFinder.ChunksMethod.ChunksMethod;
+import TestConstants.TestConstants;
 import junit.framework.TestCase;
 
 public class TestChunksMethod extends TestCase {
 
-    public void testChunksMethodOutput() {
-        int[] expectedPrimeCounts = {
-                25, // n = 100
-                168, // n = 1000
-                1229, // n = 10000
-                9592, // n = 100000
-                78498, // n = 1000000
-                664579 // n = 10000000
-        };
+    private final int[] threadCounts = TestConstants.threadCounts;
+    private final int numberOfTries = TestConstants.numberOfTries;
 
-        int[] nValues = {100, 1000, 10000, 100000, 1000000, 10000000};
+    private void outputTestHelper(int threads) {
+
+        int[] expectedPrimeCounts = TestConstants.outputTestExpectedAnswers;
+        int[] nValues = TestConstants.outputTestNs;
 
         for (int i = 0; i < nValues.length; i++) {
-            IPrimesFinder finder = new ChunksMethod(nValues[i], 1);
+            IPrimesFinder finder = new ChunksMethod(nValues[i], threads);
             int primeCount = finder.getPrimesCount();
             assertEquals(expectedPrimeCounts[i], primeCount);
         }
     }
 
-    public void testChunksMethodPerformanceOnSmallN() {
-        int[] nValues = {100, 1000, 10000, 100000, 1000000};
-        int[] threadCounts = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
-        int numRuns = 10;
-
+    private void performanceTestHelper(int[] nValues) {
         for (int n : nValues) {
             System.out.println("N = " + n);
             for (int threads : threadCounts) {
                 System.out.println("  Threads: " + threads);
                 long totalTime = 0;
-                for (int i = 0; i < numRuns; i++) {
+                for (int i = 0; i < numberOfTries; i++) {
                     IPrimesFinder finder = new ChunksMethod(n, threads);
                     long start = System.currentTimeMillis();
                     finder.getPrimesCount();
@@ -43,70 +36,39 @@ public class TestChunksMethod extends TestCase {
                     long time = (end - start);
                     totalTime += time;
                 }
-                long averageTime = totalTime / numRuns;
+                long averageTime = totalTime / numberOfTries;
                 System.out.println("  Average Time: " + averageTime + "ms");
             }
             System.out.println();
         }
+    }
+
+    public void testChunksMethodOutput() {
+        outputTestHelper(1);
+        outputTestHelper(2);
+        outputTestHelper(4);
+    }
+
+    public void testChunksMethodPerformanceOnSmallN() {
+        int[] nValues = {100, 1000, 10000, 100000, 1000000};
+        performanceTestHelper(nValues);
     }
 
     public void testChunksMethodPerformanceOn1e7() {
         int[] nValues = {10000000};
-        int[] threadCounts = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384};
-        int numRuns = 5;
-
-        for (int n : nValues) {
-            System.out.println("N = " + n);
-            for (int threads : threadCounts) {
-                System.out.println("  Threads: " + threads);
-                long totalTime = 0;
-                for (int i = 0; i < numRuns; i++) {
-                    IPrimesFinder finder = new ChunksMethod(n, threads);
-                    long start = System.currentTimeMillis();
-                    finder.getPrimesCount();
-                    long end = System.currentTimeMillis();
-                    long time = (end - start);
-                    totalTime += time;
-                }
-                long averageTime = totalTime / numRuns;
-                System.out.println("  Average Time: " + averageTime + "ms");
-            }
-            System.out.println();
-        }
+        performanceTestHelper(nValues);
     }
 
     public void testChunksMethodPerformanceOn1e8() {
         int[] nValues = {100000000};
-        int[] threadCounts = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384};
-        int numRuns = 3;
-
-        for (int n : nValues) {
-            System.out.println("N = " + n);
-            for (int threads : threadCounts) {
-                System.out.println("  Threads: " + threads);
-                long totalTime = 0;
-                for (int i = 0; i < numRuns; i++) {
-                    IPrimesFinder finder = new ChunksMethod(n, threads);
-                    long start = System.currentTimeMillis();
-                    finder.getPrimesCount();
-                    long end = System.currentTimeMillis();
-                    long time = (end - start);
-                    totalTime += time;
-                }
-                long averageTime = totalTime / numRuns;
-                System.out.println("  Average Time: " + averageTime + "ms");
-            }
-            System.out.println();
-        }
+        performanceTestHelper(nValues);
     }
 
-    public void testChunksMethodPerformanceOn1e8With1024Threads() {
-        try {
-            // Wait some time to connect using JConsole
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    public void testChunksMethodPerformanceOn1e8With1024Threads() throws InterruptedException {
+
+        // To connect with JConsole
+        Thread.sleep(10000);
+
         int[] nValues = {100000000};
         int[] threadCounts = {1024};
         int numRuns = 1;
@@ -130,4 +92,5 @@ public class TestChunksMethod extends TestCase {
             System.out.println();
         }
     }
+
 }
--- PrimesFinder/src/test/java/TestSieveOfEratosthenes/TestSieveOfEratosthenes.java
+++ PrimesFinder/src/test/java/TestSieveOfEratosthenes/TestSieveOfEratosthenes.java
@@ -2,42 +2,34 @@ package TestSieveOfEratosthenes;
 
 import PrimesFinder.Abstractions.IPrimesFinder;
 import PrimesFinder.SieveOfEratosthenes.SieveOfEratosthenes;
+
+import TestConstants.TestConstants;
 import junit.framework.TestCase;
 
 public class TestSieveOfEratosthenes extends TestCase {
 
-    public void testSieveOfEratosthenesOutput() {
-        int[] expectedPrimeCounts = {
-                25, // n = 100
-                168, // n = 1000
-                1229, // n = 10000
-                9592, // n = 100000
-                78498, // n = 1000000
-                664579 // n = 10000000
-        };
+    private final int[] threadCounts = TestConstants.threadCounts;
+    private final int numberOfTries = TestConstants.numberOfTries;
 
-        int[] nValues = {100, 1000, 10000, 100000, 1000000, 10000000};
+    private void outputTestHelper(int threads) {
+
+        int[] expectedPrimeCounts = TestConstants.outputTestExpectedAnswers;
+        int[] nValues = TestConstants.outputTestNs;
 
         for (int i = 0; i < nValues.length; i++) {
-            IPrimesFinder finder = new SieveOfEratosthenes(nValues[i], 1);
+            IPrimesFinder finder = new SieveOfEratosthenes(nValues[i], threads);
             int primeCount = finder.getPrimesCount();
             assertEquals(expectedPrimeCounts[i], primeCount);
-//            System.out.println("N= " + nValues[i]);
-//            System.out.println("p= " + primeCount);
         }
     }
 
-    public void testSieveOfEratosthenesPerformanceOn1e7() {
-        int[] nValues = {10000000};
-        int[] threadCounts = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
-        int numRuns = 10;
-
+    private void performanceTestHelper(int[] nValues) {
         for (int n : nValues) {
             System.out.println("N = " + n);
             for (int threads : threadCounts) {
                 System.out.println("  Threads: " + threads);
                 long totalTime = 0;
-                for (int i = 0; i < numRuns; i++) {
+                for (int i = 0; i < numberOfTries; i++) {
                     IPrimesFinder finder = new SieveOfEratosthenes(n, threads);
                     long start = System.currentTimeMillis();
                     finder.getPrimesCount();
@@ -45,36 +37,47 @@ public class TestSieveOfEratosthenes extends TestCase {
                     long time = (end - start);
                     totalTime += time;
                 }
-                long averageTime = totalTime / numRuns;
+                long averageTime = totalTime / numberOfTries;
                 System.out.println("  Average Time: " + averageTime + "ms");
             }
             System.out.println();
         }
+    }
+
+    public void testSieveOfEratosthenesOutput() {
+
+        int[] expectedPrimeCounts = TestConstants.outputTestExpectedAnswers;
+        int[] nValues = TestConstants.outputTestNs;
+
+        outputTestHelper(1);
+        outputTestHelper(2);
+        outputTestHelper(4);
+
+    }
+
+    public void testSieveOfEratosthenesPerformanceOnSmallN() {
+        int[] nValues = {100, 1000, 10000, 100000, 1000000};
+        performanceTestHelper(nValues);
+    }
+
+    public void testSieveOfEratosthenesPerformanceOn1e7() {
+
+        int[] nValues = {10000000};
+        performanceTestHelper(nValues);
     }
 
     public void testSieveOfEratosthenesPerformanceOn1e8() {
         int[] nValues = {100000000};
-        int[] threadCounts = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
-        int numRuns = 5;
-
-        for (int n : nValues) {
-            System.out.println("N = " + n);
-            for (int threads : threadCounts) {
-                System.out.println("  Threads: " + threads);
-                long totalTime = 0;
-                for (int i = 0; i < numRuns; i++) {
-                    IPrimesFinder finder = new SieveOfEratosthenes(n, threads);
-                    long start = System.currentTimeMillis();
-                    finder.getPrimesCount();
-                    long end = System.currentTimeMillis();
-                    long time = (end - start);
-                    totalTime += time;
-                }
-                long averageTime = totalTime / numRuns;
-                System.out.println("  Average Time: " + averageTime + "ms");
-            }
-            System.out.println();
-        }
+        performanceTestHelper(nValues);
     }
+
+    public void testSieveOfEratosthenesPerformanceOn1e9() throws InterruptedException {
+        // To connect with JConsole
+        Thread.sleep(10000);
+
+        int[] nValues = {1000000000};
+        performanceTestHelper(nValues);
+    }
+
 
 }
